b8c6383e80ca8a8305bb84e9c4c3f839ec8c24cf
GZablot <zablot.guillaume@bbox.fr>
2017-12-08 11:58:01 +0100

    maj signature

--- getSignature.m
+++ getSignature.m
@@ -1,3 +1,25 @@
-function [r, teta] = getSignature()
-
+function [r, teta] = getSignature(XCentre, YCentre, img)
+    
+    VThetaDeg = 0:1:360;
+    VTheta = VThetaDeg *pi / 180;
+    
+    % Cherche le point du contour
+    i = 1;
+    
+    while img(round(XCentre + i * cosd(VTheta)), round(YCentre + i * sind(VTheta))) == 1
+        i = i + 1; % pas du deplacement sur la droite pour aller au contour
+    end
+    
+    pointContourX = XCentre + i * cosd(VTheta);
+    pointContourY = YCentre + i * cosd(VTheta);
+    
+    insertMarker(img, [pointContourX, pointContourY]);
+    imshow(img);
+    
+    %Rayon = 5;
+    
+    %XCercle = XCentre + Rayon * cosd(VTheta);
+    
+    %YCercle = YCentre + Rayon * sind(VTheta);
+    % plot(XCercle, YCercle);
 end
---
+++ mainTest.m
@@ -0,0 +1,5 @@
+function  mainTest()
+    im = imread('C:\Users\Guillaume\Desktop\Ecole\MPRI\projetmpri\db\apple-2.gif');
+    getSignature(50, 50, im);
+end
+
