443ded0df99f566bda2ae3d216de5903f6e03fda
GZablot <zablot.guillaume@bbox.fr>
2017-12-09 14:47:29 +0100

    maj signature

--- getSignature.m
+++ getSignature.m
@@ -1,33 +1,31 @@
 function [r, teta] = getSignature(XCentre, YCentre, img)
     
-    pointContourX = []
-    pointContourY = []
+    pointContourX = [];
+    pointContourY = [];
     
-    for VThetaDeg = 0:10:360
-        VTheta = VThetaDeg *pi / 180;
-
+    for VTheta = 0:pi/5:2*pi % 0:10:360
+        % VTheta = VThetaDeg *pi / 180;
+        
         % Cherche le point du contour
         i = 1;
-
+        
+        
         while img(round(XCentre + i * cos(VTheta)), round(YCentre + i * sin(VTheta))) == 1
-            i = i + 1; % pas du deplacement sur la droite pour aller au contour
+            i = i + 10; % pas du deplacement sur la droite pour aller au contour
         end
+        
 
         pointContourX(end+1) = XCentre + i * cos(VTheta);
         pointContourY(end+1) = YCentre + i * cos(VTheta);
-        pointContourX
-        pointContourY
+        
     end
-    %insertMarker(img, [pointContourX, pointContourY]);
+    
+    % pointContourX
+    % pointContourY
     
     imshow(img);
     hold on;
     plot(pointContourX, pointContourY);
     
-    %Rayon = 5;
-    
-    %XCercle = XCentre + Rayon * cosd(VTheta);
-    
-    %YCercle = YCentre + Rayon * sind(VTheta);
-    % plot(XCercle, YCercle);
+   
 end
--- mainTest.m
+++ mainTest.m
@@ -1,7 +1,7 @@
 function  mainTest()
-    im = imread('C:\Users\Guillaume\Desktop\Ecole\MPRI\projetmpri\db\apple-2.gif');
-    coord = getBarycentre(im);
-    coord(1, 1)
-    getSignature(coord(1), coord(2), im);
+    clc;
+    im = logical(imread('C:\Users\Guillaume\Desktop\Ecole\MPRI\projetmpri\db\apple-2.gif'));
+    [x, y] = getBarycentre(im);
+    getSignature(x, y, im);
 end
 
